f62e61970fb0e9de9e034568123d4d09344690a0
Miguel <595156223@qq.com>
2020-09-04 14:31:08 +0800

    springmvc-06-ajax Login Authentication

--- springmvc-06-ajax/src/main/java/com/miguel/controller/AjaxController.java
+++ springmvc-06-ajax/src/main/java/com/miguel/controller/AjaxController.java
@@ -1,10 +1,13 @@
 package com.miguel.controller;
 
+import com.miguel.pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AjaxController {
@@ -22,5 +25,36 @@ public class AjaxController {
         } else {
             response.getWriter().println("false");
         }
+    }
+
+    @RequestMapping("/a2")
+    public List<User> a2() {
+        List<User> userList = new ArrayList<User>();
+
+        userList.add(new User("Miguel", 1, "男"));
+        userList.add(new User("Winnie", 2, "女"));
+        userList.add(new User("DuDu", 1, "男"));
+
+        return userList;
+    }
+
+    @RequestMapping("/a3")
+    public String a3(String name, String pwd) {
+        String msg = "";
+        if (name != null) {
+            if ("admin".equals(name)) {
+                msg = "OK";
+            } else {
+                msg = "用户名有误";
+            }
+        }
+        if (pwd != null) {
+            if ("123456".equals(pwd)) {
+                msg = "OK";
+            } else {
+                msg = "密码有误";
+            }
+        }
+        return msg;
     }
 }
